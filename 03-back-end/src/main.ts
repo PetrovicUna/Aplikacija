04e1e57da9f2ffb2d797b11ecd0692f5732a4d16
Una <una.petrovic@non.agilent.com>
2021-09-08 14:17:57 +0200

    Modify main.ts

--- 03-back-end/src/main.ts
+++ 03-back-end/src/main.ts
@@ -7,53 +7,58 @@ import CategoryService from './components/category/service';
 import IApplicationResources from './common/IApplicationResources.interface';
 import * as mysql2 from "mysql2/promise";
 
-const application : express.Application = express();
+async function main() {
+    const application: express.Application = express();
 
-application.use(cors({
-    origin: "http://localhost:3000",
-    credential: true,
-}));
+    application.use(cors({
+        origin: "http://localhost:3000",
+        credential: true,
+    }));
 
-application.use(
-    Config.server.static.route,
-    express.static(Config.server.static.path, {
-        index: Config.server.static.index,
-        cacheControl: Config.server.static.cacheControl,
-        maxAge: Config.server.static.maxAge,
-        etag: Config.server.static.etag,
-        dotfiles: Config.server.static.dotfiles,
-    }),
-);
-const resources: IApplicationResources = {
-    databaseConnection: await mysql2.createConnection({
-        host: Config.database.host,
-        port: Config.database.port,
-        user: Config.database.user,
-        password: Config.database.password,
-        database: Config.database.database,
-        charset: Config.database.charset,
-        timezone: Config.database.timezone,
-        supportBigNumbers: true,
-    }),
-}
+    application.use(express.json());
 
-resources.databaseConnection.connect();
+    const resources: IApplicationResources = {
+        databaseConnection: await mysql2.createConnection({
+            host: Config.database.host,
+            port: Config.database.port,
+            user: Config.database.user,
+            password: Config.database.password,
+            database: Config.database.database,
+            charset: Config.database.charset,
+            timezone: Config.database.timezone,
+            supportBigNumbers: true,
+        }),
+    }
 
-resources.services = {
-    categoryService:  new CategoryService(resources),
+    resources.databaseConnection.connect();
 
-};
+    resources.services = {
+        categoryService: new CategoryService(resources),
+    };
 
-Router.setupRoutes(application, resources, [
-    new CategoryRouter(),
-    // ...
-]);
+    application.use(
+        Config.server.static.route,
+        express.static(Config.server.static.path, {
+            index: Config.server.static.index,
+            cacheControl: Config.server.static.cacheControl,
+            maxAge: Config.server.static.maxAge,
+            etag: Config.server.static.etag,
+            dotfiles: Config.server.static.dotfiles,
+        }),
+    );
 
-application.use((req, res) => {
-    res.sendStatus(404);
-});
+    Router.setupRoutes(application, resources, [
+        new CategoryRouter(),
+        // ...
+    ]);
 
-application.use((err, req, res, next) => {
-    res.status(err.status).send(err.type);
-});
+    application.use((req, res) => {
+        res.sendStatus(404);
+    });
 
+    application.use((err, req, res, next) => {
+        res.status(err.status).send(err.type);
+    });
+
+    application.listen(Config.server.port);
+} main();
