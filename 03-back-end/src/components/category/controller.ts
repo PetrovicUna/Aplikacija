0732fbf4d351991acaac4a9a6c1e1862dd904b7e
Una <una.petrovic@non.agilent.com>
2021-09-08 12:51:39 +0200

    Add category service and controller inside main.ts

--- 03-back-end/src/components/category/controller.ts
+++ 03-back-end/src/components/category/controller.ts
@@ -1,5 +1,8 @@
 import {Request, Response, NextFunction} from "express";
 import BaseController from '../../common/BaseController';
+import CategoryService from "./service";
+import CategoryModel from './model';
+import IErrorResponse from '../../common/IErrorResponse.interface';
 
 class CategoryController extends BaseController {
     async getAll(req: Request, res: Response, next: NextFunction) {
@@ -9,6 +12,37 @@ class CategoryController extends BaseController {
         });
 
         res.send(categories);
+    }
+
+    async getById(req: Request, res: Response, next: NextFunction) {
+        const id: string = req.params.id;
+
+        const categoryId: number = +id;
+
+        if (categoryId <= 0) {
+            res.sendStatus(400);
+            return;
+        }
+
+        const data: CategoryModel|null|IErrorResponse = await this.services.categoryService.getById(
+            categoryId,
+            {
+                loadSubcategories: true,
+                loadFeatures: true,
+            }
+        );
+
+        if (data === null) {
+            res.sendStatus(404);
+            return;
+        }
+
+        if (data instanceof CategoryModel) {
+            res.send(data);
+            return;
+        }
+
+        res.status(500).send(data);
     }
 }
 
--- 03-back-end/src/main.ts
+++ 03-back-end/src/main.ts
@@ -1,5 +1,8 @@
 import * as express from "express";
 import * as cors from "cors";
+import Config from "./config/dev";
+import CategoryController from './components/category/controller';
+import CategoryService from './components/category/service';
 
 const application : express.Application = express();
 
@@ -7,6 +10,23 @@ application.use(cors({
     origin: "http://localhost:3000",
     credential: true,
 }));
+
+application.use(
+    Config.server.static.route,
+    express.static(Config.server.static.path, {
+        index: Config.server.static.index,
+        cacheControl: Config.server.static.cacheControl,
+        maxAge: Config.server.static.maxAge,
+        etag: Config.server.static.etag,
+        dotfiles: Config.server.static.dotfiles,
+    }),
+);
+
+const categoryService: CategoryService = new CategoryService();
+const categoryController: CategoryController = new CategoryController(categoryService);
+
+application.get("/category", categoryController.getAll.bind(categoryController));
+application.get("category/:id/subcategories/:sortOrder")
 
 application.use((req, res) => {
     res.sendStatus(404);
