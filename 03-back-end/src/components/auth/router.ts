9a1847861f1bb294bb98d672dbd0bc1163dd940c
Una <una.petrovic@non.agilent.com>
2021-09-10 17:34:37 +0200

    Added refresh token to controller and router

--- 03-back-end/src/components/auth/router.ts
+++ 03-back-end/src/components/auth/router.ts
@@ -9,5 +9,18 @@ export default class AuthRouter implements IRouter {
 
         application.post("/auth/user/login",            authController.userLogin.bind(authController)); 
         application.post("/auth/administrator/login",   authController.administratorLogin.bind(authController));
+
+        application.post("/auth/user/refresh",          authController.userRefresh.bind(authController));
+        application.post("/auth/administrator/refresh", authController.administratorRefresh.bind(authController));
+ 
+        application.get(
+            "/auth/user/ok",
+            authController.sendOk.bind(authController)
+        );
+
+        application.get(
+            "/auth/administrator/ok",
+            authController.sendOk.bind(authController)
+        );
     }
 }
