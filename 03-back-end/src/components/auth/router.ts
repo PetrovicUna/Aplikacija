e467a7e779620dbdea25f4771ab1ed4becf66b2e
Una <una.petrovic@non.agilent.com>
2021-09-10 17:07:23 +0200

    Added AdminLogin in controller and router

--- 03-back-end/src/components/auth/router.ts
+++ 03-back-end/src/components/auth/router.ts
@@ -8,5 +8,6 @@ export default class AuthRouter implements IRouter {
         const authController: AuthController = new AuthController(resources);
 
         application.post("/auth/user/login",            authController.userLogin.bind(authController)); 
+        application.post("/auth/administrator/login",   authController.administratorLogin.bind(authController));
     }
 }
