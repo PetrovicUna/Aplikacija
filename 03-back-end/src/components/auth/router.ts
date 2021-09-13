54e00d9943a9f75379655fceca6e224880e2960b
Una <una.petrovic@non.agilent.com>
2021-09-13 10:37:41 +0200

    Added auth middleware and updated routers

--- 03-back-end/src/components/auth/router.ts
+++ 03-back-end/src/components/auth/router.ts
@@ -2,12 +2,13 @@ import * as express from "express";
 import AuthController from './controller';
 import IApplicationResources from '../../common/IApplicationResources.interface';
 import IRouter from '../../common/IRouter.interface';
+import AuthMiddleware from '../../middleware/auth.middleware';
 
 export default class AuthRouter implements IRouter {
     public setupRoutes(application: express.Application, resources: IApplicationResources) {
         const authController: AuthController = new AuthController(resources);
 
-        application.post("/auth/user/login",            authController.userLogin.bind(authController)); 
+        application.post("/auth/user/login",            authController.userLogin.bind(authController));
         application.post("/auth/administrator/login",   authController.administratorLogin.bind(authController));
 
         application.post("/auth/user/refresh",          authController.userRefresh.bind(authController));
@@ -15,11 +16,13 @@ export default class AuthRouter implements IRouter {
  
         application.get(
             "/auth/user/ok",
+            AuthMiddleware.getVerifier("user"),
             authController.sendOk.bind(authController)
         );
 
         application.get(
             "/auth/administrator/ok",
+            AuthMiddleware.getVerifier("administrator"),
             authController.sendOk.bind(authController)
         );
     }
